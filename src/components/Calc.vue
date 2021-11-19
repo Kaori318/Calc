8e88c7c026aa8423da9c9462063ebde49f532cd2
Kaori318 <83631704+Kaori318@users.noreply.github.com>
2021-11-19 21:56:50 +0300

    lesson1

---
+++ src/components/Calc.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="display">
+      <input type="number" v-model.number="op1">
+      <input type="number" v-model.number="op2">
+      = {{ result }}
+    </div>
+    <div class="keyboard">
+      <button @click="add">+</button>
+      <button @click="sub">-</button>
+      <button @click="multiplication">*</button>
+      <button @click="division">/</button>
+      <button @click="exponentiation">**</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calc',
+  data: () => ({
+    op1: 0,
+    op2: 0,
+    result: 0,
+  }),
+  methods: {
+    add() {
+      this.result = this.op1 + this.op2;
+    },
+    sub() {
+      this.result = this.op1 - this.op2;
+    },
+    division() {
+      this.result = Math.round(this.op1 / this.op2);
+    },
+    multiplication() {
+      this.result = this.op1 * this.op2;
+    },
+    exponentiation() {
+      this.result = this.op1 ** this.op2;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
